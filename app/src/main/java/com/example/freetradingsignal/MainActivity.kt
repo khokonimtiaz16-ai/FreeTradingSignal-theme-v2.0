eb0ef2b9bfbc7cda52db5fd61ca0c0ed272c1201
khokonimtiaz16-ai <khokonimtiaz16@gmail.com>
2026-09-06 17:23:40 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/freetradingsignal/MainActivity.kt
+++ app/src/main/java/com/example/freetradingsignal/MainActivity.kt
@@ -18,45 +18,34 @@ class MainActivity : AppCompatActivity() {
         webView.settings.domStorageEnabled = true
         webView.webViewClient = WebViewClient()
 
-        val htmlData = """
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-                <style>
-                    html, body { 
-                        margin: 0; 
-                        padding: 0; 
-                        width: 100%; 
-                        height: 100%; 
-                        background-color: #131722; 
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="tradingview-widget-container" style="height:100%;width:100%">
-                  <div id="tradingview_chart" style="height:100%;width:100%"></div>
-                  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-                  <script type="text/javascript">
-                  new TradingView.widget({
-                      "autosize": true,
-                      "symbol": "BINANCE:BTCUSDT",
-                      "interval": "15",
-                      "timezone": "Etc/UTC",
-                      "theme": "dark",
-                      "style": "1",
-                      "locale": "en",
-                      "toolbar_bg": "#f1f3f6",
-                      "enable_publishing": false,
-                      "hide_side_toolbar": false,
-                      "container_id": "tradingview_chart"
-                  });
-                  </script>
-                </div>
-            </body>
-            </html>
-        """.trimIndent()
+        val htmlContent = "<html>" +
+                "<head>" +
+                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\">" +
+                "<style>html, body { margin: 0; padding: 0; width: 100%; height: 100%; background-color: #131722; }</style>" +
+                "</head>" +
+                "<body>" +
+                "<div class=\"tradingview-widget-container\" style=\"height:100%;width:100%\">" +
+                "<div id=\"tradingview_chart\" style=\"height:100%;width:100%\"></div>" +
+                "<script type=\"text/javascript\" src=\"https://s3.tradingview.com/tv.js\"></script>" +
+                "<script type=\"text/javascript\">" +
+                "new TradingView.widget({" +
+                "  \"autosize\": true," +
+                "  \"symbol\": \"BINANCE:BTCUSDT\"," +
+                "  \"interval\": \"15\"," +
+                "  \"timezone\": \"Etc/UTC\"," +
+                "  \"theme\": \"dark\"," +
+                "  \"style\": \"1\"," +
+                "  \"locale\": \"en\"," +
+                "  \"toolbar_bg\": \"#f1f3f6\"," +
+                "  \"enable_publishing\": false," +
+                "  \"hide_side_toolbar\": false," +
+                "  \"container_id\": \"tradingview_chart\"" +
+                "});" +
+                "</script>" +
+                "</div>" +
+                "</body>" +
+                "</html>"
 
-        webView.loadDataWithBaseURL("https://s3.tradingview.com", htmlData, "text/html", "UTF-8", null)
+        webView.loadDataWithBaseURL("https://s3.tradingview.com", htmlContent, "text/html", "UTF-8", null)
     }
 }
