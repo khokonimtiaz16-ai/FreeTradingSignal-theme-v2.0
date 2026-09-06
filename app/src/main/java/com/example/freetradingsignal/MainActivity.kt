81776a9fd0f233c96ff5a22f614e1beebd6efbdb
khokonimtiaz16-ai <khokonimtiaz16@gmail.com>
2026-09-06 17:44:20 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/freetradingsignal/MainActivity.kt
+++ app/src/main/java/com/example/freetradingsignal/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import com.example.freetradingsignal.R
 
 class MainActivity : AppCompatActivity() {
 
